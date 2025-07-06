0e31286f2049dc3e9e5d56586bbbd4ddc024beb8
SamarMst <samar002ms@gmail.com>
2025-07-06 18:40:04 +0100

    feat: implement NavBar component and update ContactPage import

--- client/src/pages/navbar/index.jsx
+++ client/src/pages/navbar/index.jsx
@@ -9,6 +9,9 @@ function NavBar() {
   const handleContact = () => {
     navigate("/contact-us");
   }
+  const handleHome = () => {
+    navigate("/");
+  }
   return (
     <div className="flex justify-between items-center bg-gradient-to-r from-sky-500 to-sky-300 p-4">
         <div className="flex items-center">
@@ -16,7 +19,7 @@ function NavBar() {
           <div className="text-white text-lg font-bold ml-2">Culturology</div>
         </div>
       <div className="space-x-4">
-        <button  className="text-white hover:text-gray-400">Home</button>
+        <button onClick={handleHome} className="text-white hover:text-gray-400">Home</button>
         <button  className="text-white hover:text-gray-400">About</button>
         <button onClick={handleContact} className="text-white hover:text-gray-400">Contact</button>
         <button className="bg-white text-sky-400 px-4 py-2 rounded-3xl hover:bg-gray-200" onClick={handleLogin}>Login</button>
