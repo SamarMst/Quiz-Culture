d0dd20873f670bd75122708c216fc8f4ef74ec35
SamarMst <samar002ms@gmail.com>
2025-07-06 18:37:04 +0100

    feat: update Users model schema by removing unnecessary fields and ensuring required properties

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#  Culturology
+# <img src="client/public/images/population.png" alt="Population Image" width="50" height="50"> Culturology
--- backend/src/model/Users.js
+++ backend/src/model/Users.js
@@ -2,19 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+    uid: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    score: { type: Number, default: 0 },
+    joinedAt: { type: Date, default: Date.now },
 });
 
 mongoose.exports = mongoose.model('Users', usersSchema);
